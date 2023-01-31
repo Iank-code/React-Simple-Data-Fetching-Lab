798c24f99298d5163451d057e6b67d4e9c32fc57
ianK-code <kian99564@gmail.com>
2023-01-31 11:13:32 +0300

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react'
 
 function App() {
+  const DOG_API = 'https://dog.ceo/api/breeds/image/random'
+  const [randomDog, setRandomDog] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    fetch(DOG_API)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch error");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setLoading(true);
+        setRandomDog(data.message);
+      });
+  }, [randomDog]);
+
+  if(loading === false){
+    setLoading('Loading....')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{loading}</p>
+      <img src={randomDog} alt="A Random Dog"></img>
     </div>
   );
 }
